ab2cca9fa7f722fda87a0c5f2e8fd972555b9684
GZ <zackven2020@gmail.com>
2020-11-12 20:36:35 +0800

    submit from gz

--- vendor/dcat/laravel-admin/resources/views/widgets/markdown.blade.php
+++
@@ -1,5 +0,0 @@
-<div {!! $attributes !!}><textarea style="display:none;">{!! $content !!}</textarea></div>
-
-<script require="@editor-md">
-    editormd.markdownToHTML('{{ $id }}', {!! admin_javascript_json($options) !!});
-</script>
