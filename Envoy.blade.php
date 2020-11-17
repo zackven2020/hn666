2beaeec3479f36afb05f2c0941873d056808973b
vagrant <vagrant@homestead>
2020-11-17 10:35:10 +0000

    测试定时任务是不是可以正常运行

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,28 +1,8 @@
-<<<<<<< HEAD
 @servers(['web-1' => 'root@8.134.9.30 -p62150'])
 {{--@servers(['web-1' => 'root@161.117.9.120 -p22', 'web-2'=> 'root@192.168.174.134 -p22'])--}}
 
 
 @task('wlpy', ['on' => ['web-1']])
-    {{--cd ../www/root/haidacrm/--}}
-    {{--git pull--}}
-
-    cd ../www/wwwroot/wlpy/
-    git pull
-    {{--php artisan admin:export-seed--}}
-    {{--php artisan db:seed --class=AdminTablesSeeder--}}
-    {{--php artisan migrate:refresh --seed--}}
-    {{--php artisan admin:create-user--}}
-    {{--git pull origin {{ $branch }}--}}
-    {{--composer update--}}
-    {{--php artisan migrate--}}
-@endtask
-=======
-@servers(['web-1' => 'root@8.134.9.30 -p62150'])
-{{--@servers(['web-1' => 'root@161.117.9.120 -p22', 'web-2'=> 'root@192.168.174.134 -p22'])--}}
-
-
-@task('hn666', ['on' => ['web-1']])
     {{--cd ../www/root/haidacrm/--}}
     {{--git pull--}}
 
@@ -36,4 +16,4 @@
     {{--composer update--}}
     {{--php artisan migrate--}}
 @endtask
->>>>>>> 3f65333f37ae81d6434eb1ed96b361c42c2e981b
+
